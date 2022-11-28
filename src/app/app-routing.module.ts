5b4ad838855bdfb830d8fc07295a12a56d9a3ada
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-28 17:21:09 -0500

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChileComponent } from './components/chile/chile.component';
-import { PeruComponent } from './components/peru/peru.component';
-import { UruguayComponent } from './components/uruguay/uruguay.component';
 
 const routes: Routes = [
-  { path: 'peru', component: PeruComponent },
-  { path: 'chile', component: ChileComponent },
-  { path: 'uruguay', component: UruguayComponent },
-  { path: '**', component: PeruComponent },
+  // { path: '**', component: Component },
 ];
 
 @NgModule({
